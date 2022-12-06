09160671b2894843a05a75880f686cd437dbb7eb
cozy1712 <kolawolekabiru89@gmail.com>
2022-12-06 09:29:18 +0100

    updates

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ $(document).ready(function(){
     });
 });
       
+
+function myfunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+      
+
+
+
+window.data-toggle function(event) {
+  if (!event.target.matches('.collapse')) {
+    var dropdowns = document.getElementsByClassName("collapse show");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+} 
